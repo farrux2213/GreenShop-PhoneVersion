2e02224bcbb6d7e388daf273060f81174807259a
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-05-05 11:23:01 +0500

    nothing

--- src/Item.jsx
+++ src/Item.jsx
@@ -3,7 +3,12 @@ const Navbar = () => {
   return (
     <div className="w-4/5 h-[450px] m-auto mt-6 flex justify-between gap-[52px]">
       <div className="flex-1 flex gap-[29px]">
-        <div className="w-[100px] h-full bg-gradient-to-r from-slate-50 to-slate-100"></div>
+        <div className="w-[100px] h-full bg-gradient-to-r from-slate-50 to-slate-100">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
         <div className="w-[444px] h-full bg-gradient-to-r from-slate-50 to-slate-100"></div>
       </div>
       <div className="flex-1">
