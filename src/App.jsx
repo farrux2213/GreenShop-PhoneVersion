184de1e16c559c0061b539c2a1c3d8f85d8a0001
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-05-05 11:22:33 +0500

    no commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import "./App.css";
+import Logo from "/Logo.svg";
+import Search from "/search.svg";
+import Shop from "/shop.svg";
+import { LoginOutlined } from "@ant-design/icons";
+import { Navbar } from "./Item";
+
+function App() {
+  return (
+    <>
+      <div className=" w-[80%] h-[78px] m-auto flex items-center justify-between border-b-[1.5px] border-lime-500  border-solid ">
+        <div className="">
+          <img src={Logo} className=" cursor-pointer" alt="no img" />
+        </div>
+        <div className="flex gap-[30px] m-auto">
+          <h3 className="font-sans text-base font-normal text-left relative transition-shadow ">
+            Home
+          </h3>
+          <h3 className="font-sans text-base font-normal text-left relative transition-shadow ">
+            Shop
+          </h3>
+          <h3 className="font-sans text-base font-normal text-left relative transition-shadow ">
+            Plant Care
+          </h3>
+          <h3 className="font-sans text-base font-normal text-left relative transition-shadow ">
+            Blogs
+          </h3>
+        </div>
+        <div className="flex gap-[30px]">
+          <img src={Search} alt="no img" className="cursor-pointer" />
+          <img src={Shop} alt="no img" className="cursor-pointer" />
+          <button className="w-[100px] h-[35px] rounded-md bg-lime-600 flex gap-[6px] items-center justify-center">
+            <LoginOutlined className="text-gray-50" />
+            <p className="text-gray-50">Login</p>
+          </button>
+        </div>
+      </div>
+      <div className=" flex items-center w-4/5 h-[50px] m-auto mt-[20px] ">
+        <h1 className="font-sans font-normal text-base">
+          <span className="font-sans font-bold text-base">Home</span> / Shop
+        </h1>
+      </div>
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Item.jsx
@@ -0,0 +1,29 @@
+import { Rate } from "antd";
+const Navbar = () => {
+  return (
+    <div className="w-4/5 h-[450px] m-auto mt-6 flex justify-between gap-[52px]">
+      <div className="flex-1 flex gap-[29px]">
+        <div className="w-[100px] h-full bg-gradient-to-r from-slate-50 to-slate-100"></div>
+        <div className="w-[444px] h-full bg-gradient-to-r from-slate-50 to-slate-100"></div>
+      </div>
+      <div className="flex-1">
+        <h1 className="font-sans font-bold text-3xl">Barberton Daisy</h1>
+        <div className="w-full h-[50px] border-b mt-[14px] flex justify-between">
+          <div className="w-[87px] flex items-center text-left">
+            <h1 className="font-sans font-bold text-xl text-green-700">
+              $119.00
+            </h1>
+          </div>
+          <div className="w-auto flex items-center justify-center gap-[11px]">
+            <Rate />
+            <p className="font-sans text-sm font-normal w-[145px]">
+              19 Customer Review
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Navbar };
